f36b17cdecbcf30d9a859cc59232148494643678
Jalil KETTANI <jkettani@e2r2p3.42.fr>
2018-11-15 11:59:06 +0100

    Modify launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -6,15 +6,46 @@ if [[ $# -eq 0 ]]; then
 fi
 
 # Variables
+CC="gcc"
 CFLAGS="-Wall -Werror -Wextra"
+CPPFLAGS="-I."
+LFLAGS="-L. -lft"
+COMPILE = ${CC} ${CFLAGS} ${CPPFLAGS}
+
+# Get function name
+TEST_FCT="$1"
+
+# Compilation
+${COMPILE} tests/${TEST_FCT}_test.c -o out/${TEST_FCT}_test
+
+# Print function and parameters
+function	print_params {
+
+}
+
+# Error output
+function error_output {
+	if [ ${ERRCODE} -eq 0 ]; then
+		 printf "%s(%s, %d, %d): %s\n" $1 $2 $3 $4 "No error (0)" 
+	elif [ ${ERRCODE} -eq 139 ]; then
+		 printf "%s(%s, %d, %d): %s\n" $1 $2 $3 $4 "Segfault (${ERRCODE})"
+	elif [ ${ERRCODE} -eq 134 ]; then
+		 printf "%s(%s, %d, %d): %s\n" $1 $2 $3 $4 "Abort (${ERRCODE})"
+	fi
+}
+
+
+# bzero
+if [ ${TEST_FCT} = "bzero" ]; then
+	
+fi
 
 # memset
-if [ "$1" = "memset" ]; then
-	TEST_FCT="memset"
-	gcc -Wall -Werror -Wextra  -I. -L. -lft tests/base_memset_test.c -o out/base_memset_test
-	function base_test_memset {
+if [ ${TEST_FCT} = "memset" ]; then
+	${COMPILE} tests/${TEST_FCT}_test.c -o out/${TEST_FCT}_test
+	function memset_test {
 		{
-		./out/base_memset_test $2 $3 $4
+		./out/${TEST_FCT}_test $2 $3 $4
 		ERRCODE=$?
 		} #2> /dev/null
 		if [ ${ERRCODE} -eq 0 ]; then
@@ -30,19 +61,14 @@ if [ "$1" = "memset" ]; then
 		printf "" > $fct\_errors.txt
 		for b in NULL unsigned_char[100] # test NULL pointer and byte string
 		do
-			for char in 97
+			for char in 0 97
 			do
 				for len in -2 150
 				do
-					base_test_memset $fct $b $char $len >> $fct\_errors.txt
+					base_test_memset $fct $b $char $len >> $fct\_output.txt
 				done
 			done
 		done
 	done
-	cat ${TEST_FCT}_errors.txt
-fi
-
-if [ "$1" = "memset2" ]; then
-	gcc -Wall -Werror -Wextra  -I. -L. -lft tests/base_memset_test.c -o out/base_memset_test
-	./out/base_memset_test
+	cat ${TEST_FCT}_output.txt
 fi
