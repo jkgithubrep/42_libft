be15a720b4ff022c203a59e47003bc4c953666ca
Jalil KETTANI <jkettani@e2r2p3.42.fr>
2018-11-28 19:19:25 +0100

    Norm ft_putnbr_base

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -6,13 +6,13 @@
 /*   By: jkettani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/18 13:15:18 by jkettani          #+#    #+#             */
-/*   Updated: 2018/11/28 18:41:31 by jkettani         ###   ########.fr       */
+/*   Updated: 2018/11/28 19:17:48 by jkettani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_base(int nbr, int	base_size)
+void	ft_putnbr_base(int nbr, int base_size)
 {
 	long	n;
 	char	*base;
