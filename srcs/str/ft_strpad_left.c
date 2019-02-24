fbb27c8adb39de7d79a6cb8e3b4fb99fca259ddc
Jalil KETTANI <jkettani@e2r2p3.42.fr>
2019-02-24 20:54:33 +0100

    Remove test main from ft_strpad_left.c

--- srcs/str/ft_strpad_left.c
+++ srcs/str/ft_strpad_left.c
@@ -6,7 +6,7 @@
 /*   By: jkettani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/24 18:44:20 by jkettani          #+#    #+#             */
-/*   Updated: 2019/02/24 20:44:04 by jkettani         ###   ########.fr       */
+/*   Updated: 2019/02/24 20:53:31 by jkettani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,4 @@ char		*ft_strpad_left(char **str, int c, size_t len)
 	ft_strprepend(prepend, str);
 	ft_strdel(&prepend);
 	return (*str);
-}
-
-int		main(void)
-{
-	char 	*str;
-	char	*ret;
-
-	str = ft_strdup("chaine12");
-	printf("%s\n", str);
-	ret = ft_strpad_left(&str, '_', 10);
-	printf("%s\n", str);
-	ft_strdel(&str);
 }
